dd642b64df565b0842c806e108a9db035b63277a
Aditya Prasetyo <adty404@gmail.com>
2022-05-16 23:23:15 +0700

    Constructor Property Promotion

---
+++ ConstructorPropertyPromotion.php
@@ -0,0 +1,14 @@
+<?php
+
+class Order{
+
+    public function __construct(Public string $id, Public string $sender, Public string $receiver, Private int $quantity, Private string $item)
+    {}
+
+    public function getOrder(){
+        return "order id {$this->id} dengan pengirim bernama {$this->sender} dan penerimanya bernama {$this->receiver}";
+    }
+}
+
+$detail_order = new Order(id: "1", sender: "Achmad", receiver: "Fauzi", quantity: 2, item: "buku");
+var_dump($detail_order);
